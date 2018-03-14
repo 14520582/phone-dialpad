360af782ae7aec13cad49ca74192c86c786fad27
nghia <bikinluacongtu@gmail.com>
2018-03-14 17:25:21 +0700

    f

---
+++ src/app/component/dialpad-button/dialpad-button.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-dialpad-button',
+  templateUrl: './dialpad-button.component.html',
+  styleUrls: ['./dialpad-button.component.scss']
+})
+export class DialpadButtonComponent implements OnInit {
+  @Input() main: string;
+  @Input() letter: string;
+  constructor() {
+
+  }
+  ngOnInit() {
+  }
+
+}
